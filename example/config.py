1a5c2af8cf04c92a9808bb4e4f5a99ff8805833d
Pranjal Kushwaha <pranjal0819@gmail.com>
2024-08-27 16:06:40 +0530

    Add Example Document for Legal Company

---
+++ example/config.py
@@ -0,0 +1,60 @@
+# Configuration
+OPTIONS = {
+    "0": "Blank Document Generation",
+    "1": "AGM Notice Document Generation",
+    "2": "Attendance Register Document Generation",
+    "3": "Attendance Slip Document Generation",
+    "4": "Compliance of Section 137 Document Generation",
+    "5": "Declaration for Submission of Form Document Generation",
+    "6": "Directors Report Document Generation",
+    "7": "List of Directors Document Generation",
+    "8": "List of Shareholders Document Generation",
+    "9": "MGT 9 Document Generation",
+    "10": "Minutes Final Document Generation",
+    "11": "Notice Agenda of BM Document Generation",
+}
+
+CHOICE_MAPPING = {
+    "0": "template",
+    "1": "agm_notice",
+    "2": "attendance_register",
+    "3": "attendance_slip",
+    "4": "compliance_of_section_137",
+    "5": "declaration_for_submission_of_form",
+    "6": "director_report",
+    "7": "list_of_directors",
+    "8": "list_of_shareholders",
+    "9": "mgt_9",
+    "10": "minutes_final",
+    "11": "notice_agenda_of_bm",
+}
+
+CSV_PATH = {
+    "agm_notice": "data/agm_notice.csv",
+    "attendance_register": "data/attendance_register.csv",
+    "attendance_slip": "data/attendance_slip.csv",
+    "compliance_of_section_137": "data/compliance_of_section_137.csv",
+    "declaration_for_submission_of_form": "data/declaration_for_submission_of_form.csv",
+    "director_report": "data/director_report.csv",
+    "list_of_directors": "data/list_of_directors.csv",
+    "list_of_shareholders": "data/list_of_shareholders.csv",
+    "mgt_9": "data/mgt_9.csv",
+    "minutes_final": "data/minutes_final.csv",
+    "notice_agenda_of_bm": "data/notice_agenda_of_bm.csv",
+    "template": "data/template.csv",
+}
+
+TEMPLATE_PATH = {
+    "agm_notice": "templates/agm_notice.docx",
+    "attendance_register": "templates/attendance_register.docx",
+    "attendance_slip": "templates/attendance_slip.docx",
+    "compliance_of_section_137": "templates/compliance_of_section_137.docx",
+    "declaration_for_submission_of_form": "templates/declaration_for_submission_of_form.docx",
+    "director_report": "templates/director_report.docx",
+    "list_of_directors": "templates/list_of_directors.docx",
+    "list_of_shareholders": "templates/list_of_shareholders.docx",
+    "mgt_9": "templates/mgt_9.docx",
+    "minutes_final": "templates/minutes_final.docx",
+    "notice_agenda_of_bm": "templates/notice_agenda_of_bm.docx",
+    "template": "templates/template.docx",
+}
